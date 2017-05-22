01cda19b2c990506977ae1c565b0db906e1dcd5e
furtaw.3@wright.edu <furtaw.3@wright.edu>
2017-05-22 14:02:58 -0400

    Finish conversion of previous site into new template

---
+++ legacy/js/nav.js
@@ -0,0 +1,21 @@
+document.write('\
+	<nav>\
+		<div class="w3-row">\
+			<div class="iconbar">\
+				<map name="hiddenbutton">\
+					<area shape="rect" coords="20,20,30,30" href="secret.html" />\
+				</map>\
+				<img src="transparent.png" usemap="#hiddenbutton" />\
+			</div>\
+			<div class="navbar">\
+				<h1><a class="sitetitle" href="index.html">Samuel Furtaw</a></h1>\
+				<ul class="links">\
+					<script type="text/javascript" src="legacy/js/links.js"></script>\
+				</ul>\
+				<ul class="socialbar">\
+					<script type="text/javascript" src="legacy/js/socialbar.js"></script>\
+				</ul>\
+			</div>\
+		</div>\
+	</nav>\
+');
